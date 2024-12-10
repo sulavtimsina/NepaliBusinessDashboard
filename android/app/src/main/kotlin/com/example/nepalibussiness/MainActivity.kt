8a2cc33abc1c756904c85a77ee037e6a11f99945
Hizar62 <hizarsajjad632@gmail.com>
2024-12-10 12:24:42 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/nepalibussiness/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nepalibussiness
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/UI/registration.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Registration extends StatelessWidget {
+  const Registration({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
