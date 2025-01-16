9765736a0c9f8ba797eb8a2b199f102e3b876cf4
Sulav <sulavtimsina@users.noreply.github.com>
2025-01-15 23:18:40 -0600

    Updated app name to KhojBiz

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Utils {
   final pimaryColor = const Color(0xffC2E96A);
-  final String name = 'Nepali Business';
+  final String name = 'KhojBiz';
   final String pageName = 'Admin Dashboard';
   final String reset = 'Forget Password';
   final textColor = const Color(0xff114c2b);
